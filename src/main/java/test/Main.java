67acb1f10f073c65385f067d39e0558a317e9099
fang <fangcl@nuaa.edu.cn>
2024-04-03 00:51:26 +0800

    Initial commit

---
+++ src/main/java/test/Main.java
@@ -0,0 +1,16 @@
+package test;
+
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args) {
+
+        int[] arr = {2,4,9};
+
+        int num = 23121;
+        String s = "" + num;
+        int length = s.length();
+    }
+
+    
+}
